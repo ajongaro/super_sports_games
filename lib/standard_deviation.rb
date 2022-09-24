be6d189e51072b5285c99b65f98b24fc63915a59
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-09-24 13:20:45 -0700

    Add standard_deviation class

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,13 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+class StandardDeviation
+  attr_reader :result;
 
-# When you find the standard deviation, print it out
+  def initialize(array)
+    sum_divided_by_size = (array.sum.to_f / array.size)
+    array.map! { |age| (age - sum_divided_by_size)**2 }
+    @result = Math.sqrt(array.sum / array.size)
+  end
+end
+
+p StandardDeviation.new(ages).result
